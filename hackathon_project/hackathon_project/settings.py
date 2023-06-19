d696cb84eafe6526eb9fda9705298dbf1ee5304c
Himanshu Bobade <himanshubobade96@gmail.com>
2023-06-19 13:48:22 +0530

    connected to postgres database

--- hackathon_project/hackathon_project/settings.py
+++ hackathon_project/hackathon_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hackathon_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aiplanetdb',
+        'USER': 'himanshu1234',
+        'PASSWORD': 'himanshu1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
