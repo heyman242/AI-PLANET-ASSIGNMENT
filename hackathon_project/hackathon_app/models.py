71d0767e83044673e1633e7f54a50cd3d61f7068
Himanshu Bobade <himanshubobade96@gmail.com>
2023-06-21 20:56:13 +0530

    created RegisterUser table for authentication

--- hackathon_project/hackathon_app/models.py
+++ hackathon_project/hackathon_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class RegisteredUser(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=10, unique=True)
+    email = models.EmailField(blank=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
